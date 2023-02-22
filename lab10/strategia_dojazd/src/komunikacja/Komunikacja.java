0d4f6a0a1987ac7516b87edc64a9681682b180cc
olekszczepanowski <120830093+olekszczepanowski@users.noreply.github.com>
2023-02-22 18:54:28 +0100

    Delete lab10/strategia_dojazd directory

--- lab10/strategia_dojazd/src/komunikacja/Komunikacja.java
+++
@@ -1,8 +0,0 @@
-package komunikacja;
-
-public interface Komunikacja {
-    double obliczKoszt(double dystans);
-    int obliczPrzesiadki(double dystans);
-
-    double obliczCzas(double dystans);
-}
--- lab10/strategia_dojazd/src/samodzielnie/Samodzielnie.java
+++
@@ -1,6 +0,0 @@
-package samodzielnie;
-
-public interface Samodzielnie {
-    double obliczCzas(double dystans);
-    double obliczKalorie(double dystans);
-}
